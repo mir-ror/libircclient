b9034b352a9e0b4b714f06ef4b42d36306d54dd9
gyunaev <gyunaev@28a50672-2c1c-0410-871c-d3b6e71dd7f9>
2004-10-17 17:46:17 +0000

    DEVEL: prepare to 0.8
    git-svn-id: svn://svn.code.sf.net/p/libircclient/code/trunk/libircclient@57 28a50672-2c1c-0410-871c-d3b6e71dd7f9

--- src/win32/make_distro.sh
+++ src/win32/make_distro.sh
@@ -12,13 +12,13 @@ cp ../../include/libirc_events.h  win32/include/
 cp ../../examples/* win32/examples/
 cp spammer.dsp win32/examples/
 cd win32
-zip -r ../libircclient-win32-vc-0.7.zip .
+zip -r ../libircclient-win32-vc-0.8.zip .
 cd ..
 rm -rf win32
 
 mkdir doc
 cp ../../doc/html/* doc/
 cd doc
-zip -r ../libircclient-dochtml-0.7.zip .
+zip -r ../libircclient-dochtml-0.8.zip .
 cd ..
 rm -rf doc
